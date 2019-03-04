8dd4d1421bd3c028033ac3753b631a45356eb79f
Alex <hanson@cs.umd.edu>
2019-03-03 22:20:10 -0500

    not distributed 4 gpu test

--- imagenet/run_scripts/resnet50.sh
+++ imagenet/run_scripts/resnet50.sh
@@ -5,4 +5,4 @@
 #SBATCH --qos=high
 #SBATCH --time=1-12:00:00
 
-srun python main.py -a resnet50 --lr 0.01 --dist-url 'tcp://127.0.0.1:9999' --dist-backend 'nccl' --multiprocessing-distributed --world-size 1 --rank 0 /fs/vulcan-scratch/datasets/imagenet/
+srun python ../main.py -a resnet50 --lr 0.01 /fs/vulcan-scratch/datasets/imagenet/
