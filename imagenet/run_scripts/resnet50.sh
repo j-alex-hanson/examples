a8c4f81c8a27c847a32f30fe162d7f2822d8474b
Alex <hanson@cs.umd.edu>
2019-04-11 22:59:10 -0400

    Hack to pass in parameters to sbatch

--- imagenet/run_scripts/resnet50.sh
+++ imagenet/run_scripts/resnet50.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-#SBATCH --job-name=hard_data_baseline
-#SBATCH --output=logs/resnet50.$1.13.log
-#SBATCH --gres=gpu:4
-#SBATCH --qos=high
-#SBATCH --time=1-12:00:00
-#SBATCH --mem=128gb
+source activate pytorch
 
-srun python ../main.py -a resnet50 --lr 0.01 --resume checkpoint.resnet50.$1.tar --checkpoint-root resnet50.$1 /fs/vulcan-scratch/datasets/imagenet/
+percent=$1
+num=$2
+
+cat > resnet50.tmp.sh <<EOF
+#!/bin/bash
+srun python \
+  ../main.py \
+    -a resnet50 \
+    --lr 0.01 \
+    --resume checkpoint.resnet50.$percent.tar \
+    --checkpoint-root resnet50.$percent \
+    /fs/vulcan-scratch/datasets/imagenet/
+EOF
+
+chmod +x resnet50.tmp.sh
+
+#SBATCH commands
+sbatch \
+       --output=logs/resnet50.$percent.$num.log \
+       --job-name=hard_data_baseline \
+       --gres=gpu:4 \
+       --qos=high \
+       --time=1-12:00:00 \
+       --mem=128gb \
+       --export=all \
+       resnet50.tmp.sh
+
+rm resnet50.tmp.sh
